a43a8e8909b569227f516bea1da54d84fa3e4214
Kateryna-Katya <katyaukr2014@gmail.com>
2024-06-05 13:47:13 +0300

    add

--- js/getElementWidth.js
+++
@@ -1,7 +0,0 @@
-function getElementWidth(content, padding, border) {
-    let contentWidth = parseFloat(content);
-    let paddingWidth = parseFloat(padding);
-    let borderWidth = parseFloat(border);
-    let totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totalWidth;
-}
