20de11b5434f1fe939eb9c6f46b6b9eef44b5de6
Kateryna-Katya <katyaukr2014@gmail.com>
2024-06-05 14:14:22 +0300

    add

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+/* ========================================================================================================================================================
+console.log(makeTransaction(5, 3000));
+VM91:1 You ordered 5 droids worth 15000 credits!
+undefined
+console.log(makeTransaction(3, 1000));
+VM95:1 You ordered 3 droids worth 3000 credits!
+undefined
+console.log(makeTransaction(10, 500));
+VM99:1 You ordered 10 droids worth 5000 credits!
+======================================================================================================================================================== */
