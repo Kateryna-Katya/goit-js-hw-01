68c651968ad9d69f061d7c1fc9652b0d73922e07
Kateryna-Katya <katyaukr2014@gmail.com>
2024-06-05 20:11:05 +0300

    add initial fils

--- js/task-1.js
+++ js/task-1.js
@@ -6,13 +6,3 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
-/* ========================================================================================================================================================
-console.log(makeTransaction(5, 3000));
-VM91:1 You ordered 5 droids worth 15000 credits!
-undefined
-console.log(makeTransaction(3, 1000));
-VM95:1 You ordered 3 droids worth 3000 credits!
-undefined
-console.log(makeTransaction(10, 500));
-VM99:1 You ordered 10 droids worth 5000 credits!
-======================================================================================================================================================== */
--- js/task-3.js
+++ js/task-3.js
@@ -8,14 +8,3 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
-/* ========================================================================================================================================================
-console.log(getElementWidth("50px", "8px", "4px"));
-
-VM740:1 74
-undefined
-console.log(getElementWidth("60px", "12px", "8.5px"));
-VM744:1 101
-undefined
-console.log(getElementWidth("200px", "0px", "0px"));
-VM748:1 200
-======================================================================================================================================================== */
