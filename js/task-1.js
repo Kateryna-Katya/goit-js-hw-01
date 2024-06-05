b4b8636fe4582d110946b2023f1a8509dcb3c2cf
Kateryna-Katya <katyaukr2014@gmail.com>
2024-06-05 20:07:55 +0300

    add initial fils

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+
 }
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
 /* ========================================================================================================================================================
 console.log(makeTransaction(5, 3000));
 VM91:1 You ordered 5 droids worth 15000 credits!
